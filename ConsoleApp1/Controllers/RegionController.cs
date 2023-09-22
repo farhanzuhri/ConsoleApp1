34ea93e671535771263bd9a3f6ea74915e5d1025
farhanzuhri <101404632+farhanzuhri@users.noreply.github.com>
2023-09-22 13:47:59 +0700

    Update RegionController.cs

--- ConsoleApp1/Controllers/RegionController.cs
+++ ConsoleApp1/Controllers/RegionController.cs
@@ -4,9 +4,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Controllers
-{
-    internal class RegionController
+
+namespace ConsoleApp1.Controllers;
+    public class RegionController
     {
+        private Region _region;
+        private RegionView _regionView;
+
+        public RegionController(Region region, RegionView regionView)
+        {
+            _region = region;
+            _regionView = regionView;
+        }
+
+        public void GetAll()
+        {
+            var results = _region.GetAll();
+            if (!results.Any())
+            {
+                Console.WriteLine("No data found");
+            }
+            else
+            {
+            _regionView.List(results, "regions");
+            }
+        }
+
+        public void Insert()
+        {
+            string input = "";
+            var isTrue = true;
+            while (isTrue)
+            {
+                try
+                {
+                    input = _regionView.InsertInput();
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("Region name cannot be empty");
+                        continue;
+                    }
+                    isTrue = false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            var result = _region.Insert(new Region
+            {
+                Id = 0,
+                Name = input
+            });
+
+            _regionView.Transaction(result);
+        }
+
+        public void Update()
+        {
+            var region = new Region();
+            var isTrue = true;
+            while (isTrue)
+            {
+                try
+                {
+                    region = _regionView.UpdateRegion();
+                    if (string.IsNullOrEmpty(region.Name))
+                    {
+                        Console.WriteLine("Region name cannot be empty");
+                        continue;
+                    }
+                    isTrue = false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            var result = _region.Update(region);
+            _regionView.Transaction(result);
+        }
     }
-}
